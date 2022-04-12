07558d597c0224935183c7f22be862851c7ff903
JnathanCarter <jcarter20@stu.jsu.edu>
2022-04-12 14:42:19 -0500

    get data for select elments completed

--- script.js
+++ script.js
@@ -3,40 +3,77 @@ var Project3 = (function () {
         var searchurl = "http://ec2-3-143-211-101.us-east-2.compute.amazonaws.com/CS325_Project3/search?";
         var listurl = "http://ec2-3-143-211-101.us-east-2.compute.amazonaws.com/CS325_Project3/list?";
 
-        var subjects, scheduleTypes, InstructionalMethods, instructors, attriubuteTypes = null;
+        var jsonsubjects, jsonscheduleTypes = null;
 
         return {
                 start: function () {
                         var that = this;
 
                         // get all the data from api to populate the forms
-                        $.when(
-                                $.ajax({
 
-                                }),
+                        $.ajax({
+                                url: 'http://ec2-3-143-211-101.us-east-2.compute.amazonaws.com/CS325_Project3/list?type=subject',
+                                method: 'GET',
+                                dataType: 'json',
+                                success: function (response) {
+                                        jsonsubjects = response;
+                                        console.log("jsonsubjects---" + jsonsubjects);
+                                        that.initSubjects();
 
-                                $.ajax({
+                                }
 
-                                }),
+                        })
+                        $.ajax({
+                                url: 'http://ec2-3-143-211-101.us-east-2.compute.amazonaws.com/CS325_Project3/list?type=scheduletype',
+                                method: 'GET',
+                                dataType: 'json',
+                                success: function (response) {
+                                        jsonscheduleTypes = response;
+                                        console.log("scheduletypes---" + jsonscheduleTypes);
+                                        that.initSchedule();
 
-                                $.ajax({
-
-                                }),
-
-                                $.ajax({
-
-                                }),
-
-                                $.ajax({
-
-                                }),
-
-                        ).then(that.init(), that.fail);
+                                }
+                        })
 
                 },
 
-                /*Initialize the subjects, schedule types mthods and insturctor lists*/
-                init: function () {
+
+                /*Initialize the jsonsubjects, schedule types mthods and insturctor lists*/
+                initSubjects: function () {
+                        for (var i = 0; i < jsonsubjects[0].length; i++) {
+
+                                $("#subj_id").append("<option value=" + jsonsubjects[0][i] + ">" + jsonsubjects[1][i]);
+                        }
+                },
+                initSchedule: function () {
+                        for (var i = 0; i < jsonscheduleTypes[0].length; i++) {
+
+                                $("#scheduletypeid").append("<option value=" + jsonscheduleTypes[0][i] + ">" + jsonscheduleTypes[1][i]);
+                        }
+                },
+                fail: function () {
+
+                },
+
+                //output results
+                outputResults: function () {
+
+                },
+
+                //get info from the user and get info from api
+                onSubmit: function () {
+                        alert("this is on submit");
+                },
+
+
+                //reset the page
+                reset: function () {
+                        let confirmAction = confirm("Would you like to reset your selections?");
+                        if (confirmAction) {
+                                window.location.reload();
+                        } else {
+                                alert("Reset canceled");
+                        }
 
                 },
 
